4211eba501edcf427f5c3fb32f0f9aadb7268403
franciscorcb <franciscostay177@gmail.com>
2025-02-19 14:35:01 -0300

    continuando pagina de programacao

--- header.php
+++ header.php
@@ -17,36 +17,12 @@
             <nav id="nav-menu-container">
                 <ul class="nav-menu">
                     <li><a href="index.php">Home</a></li>
-                    <!-- <li class="menu"><a href="inscricoes.php">Inscrições</a></li> -->
+                    <li class="menu"><a href="inscricoes.php">Inscrições</a></li>
                     <li><a href="programacao.php">Programação</a></li>
                     <li><a href="localizacao.php">Localização</a></li>
                     <li><a href="materiais.php">Guias</a></li>
                     <li><a href="faq.php">FAQ</a></li>
                     <li><a href="registros.php">Registros</a></li>
-                    <!--<li class="dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" data-toggle="dropdown"
-                            role="button" aria-haspopup="true" aria-expanded="false">Versões</a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="index.php">2024.2</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="2024.1/index.php">2024.1</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="2023.2/index.php">2023.2</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="2023.1/index.php">2023.1</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="2022.2/index.php">2022.2</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="2022.1/index.php">2022.1</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="2021.2/index.php">2021.2</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="2021.1/index.php">2021.1</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="2020.2/index.php">2020.2</a>
-                        </div>
-                        </a>
-                    </li>-->
                 </ul>
             </nav>
             <!-- #nav-menu-container -->
